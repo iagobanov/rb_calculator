81cb3199fccb5de95202a2202026445948e67a39
iagobanov <iagobanov@outlook.com>
2016-08-29 20:48:28 -0300

    Add operação matematica

--- calculator.rb
+++ calculator.rb
@@ -5,4 +5,33 @@ def hello_user
 	puts "\n" + "Sou uma calculadora com funções simples(Adição,Subtração,Divisão e Multiplicaçao)."
 end
 
+def operation
+        
+        puts "\n" + "Digite o primeiro número da operação:"
+        num1=gets.to_i
+        puts "Digite o segundo número da operação:"
+        num2=gets.to_i
+        puts "Escolha a operação que deseja realizar.('+' para Adição. '-' para Subtração. '/' para Divisão. '*' para Multiplicaçao)"
+        
+        op=gets.chomp
+    
+    case op
+        when '+'
+            soma=num1+num2
+            puts "#{num1+num2}"
+        when '-'
+            sub=num1-num2
+            puts "#{sub}"
+        when '*'
+            mult= num1*num2
+            puts "#{mult}"
+        when '/'
+            div=num1/num2
+            puts "#{div}"
+        else
+            puts "invalid operator"
+    end 
+end        
+
 hello_user
+operation
